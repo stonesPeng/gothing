b0e68e5ea1108234b648e9170a0f0e989413624a
honor_stone <honor_stone@163.com>
2021-07-14 11:23:26 +0800

    recover

---
+++ basic/recover_exec.go
@@ -0,0 +1,22 @@
+/**
+  @author: honor
+  @since: 2021/7/12
+  @desc: //TODO
+**/
+package basic
+
+import "fmt"
+
+func foo() int {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	m := 1
+	panic("foo:fail")
+	m = 2
+	return m
+
+}
---
+++ basic/recover_test.go
@@ -0,0 +1,16 @@
+/**
+  @author: honor
+  @since: 2021/7/12
+  @desc: //TODO
+**/
+package basic
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRecover(t *testing.T) {
+	n := foo()
+	fmt.Println("received ", n)
+}
